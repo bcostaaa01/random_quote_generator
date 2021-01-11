246fa3607d5a2557963862551dd1f939a8ddbe68
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-01-11 14:13:48 +0000

    random number var added

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// quotes - from https://dzone.com/articles/best-programming-jokes-amp-quotes
+
+var quotes = [
+"Any fool can write code that a computer can understand. Good programmers write code that humans can understand. – Martin Fowler",
+"First, solve the problem. Then, write the code. – John Johnson",
+"Experience is the name everyone gives to their mistakes. – Oscar Wilde",
+"Java is to JavaScript what car is to Carpet. – Chris Heilmann",
+"Knowledge is power. – Francis Bacon",
+"Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code. – Dan Salomon"
+]
+
+// Math.floor() with Math.random() passes as a parameter
+
+var randomNumber = Math.floor(Math.random() * (quotes.length));
