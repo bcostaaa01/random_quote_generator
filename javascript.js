a3f907ecd2f252e783f4e0cc29772fadbf795f04
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-01-11 14:17:58 +0000

    syntax corrected on newQuote() function

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ var quotes = [
 "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code. – Dan Salomon"
 ]
 
-function newNumber = { // newNumber function to generate the quotes and display them on index.html
+function newQuote = { // newNumber function to generate the quotes and display them on index.html
 
 // Math.floor() with Math.random() passes as a parameter
 
